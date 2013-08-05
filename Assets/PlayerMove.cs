c25a7fc43dc9984400ca0740030bed17a89b449e
ChristianT <ChristianT@onlyentertainment.de>
2013-08-05 18:10:45 +0200

    tt

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 playerPosition = gameObject.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.W)) {playerPosition.z += 5;}
+        if (Input.GetKeyDown(KeyCode.S)) { playerPosition.z -= 5; }
+        if (Input.GetKeyDown(KeyCode.D)) { playerPosition.x += 5; }
+        if (Input.GetKeyDown(KeyCode.A)) { playerPosition.x -= 5; }
+
+        gameObject.transform.position = playerPosition;
+
+	}
+}
